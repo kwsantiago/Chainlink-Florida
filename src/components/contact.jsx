fefcc09fb5ab6882c35854a9635a210d46b2ac10
kwsantiago <kwsantiago@usf.edu>
2020-11-13 00:16:36 -0500

    Update UI

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -9,7 +9,7 @@ export class Contact extends Component {
             <div className="col-md-8">
               <div className="row">
                 <div className="section-title">
-                  <h2>Get In Touch</h2>
+                  <h2>Link Up</h2>
                   <p>
                     Please fill out the form below to send us an email and we
                     will get back to you as soon as possible.
